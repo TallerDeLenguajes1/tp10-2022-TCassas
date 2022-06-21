216d81d55b2687ae29a0e3ded2de8ef211b45a5c
Tomás <tomascassas@gmail.com>
2022-06-21 16:55:06 -0300

    Get civilizations

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace tp10_2022_TCassas {
+    class Program {
+        static void Main(string[] args) {
+            var request = (HttpWebRequest)WebRequest.Create("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations");
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            request.Accept = "application/json";
+
+            try {
+                using (WebResponse response = request.GetResponse()) {
+                    using (Stream strReader = response.GetResponseStream()) {
+                        using (StreamReader objReader = new StreamReader(strReader)) {
+                            string responseBody = objReader.ReadToEnd();
+
+                            var civilizationsList = JsonSerializer.Deserialize<CivilizationsList>(responseBody);
+
+                            foreach(Civilization civilization in civilizationsList.Civilizations) {
+                                Console.WriteLine(civilization.Id);
+                            }
+                        }
+                    }
+                }
+            } catch (WebException ex) {
+                Console.WriteLine("alto error");
+            }
+        }
+    }
+}
